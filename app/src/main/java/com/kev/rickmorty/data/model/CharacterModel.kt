f6142c9b349a264ba5becd84db670171fa5c61ac
kev87ian <kelvinian87@gmail.com>
2022-07-08 15:09:05 +0300

    designed the character item

--- app/src/main/java/com/kev/rickmorty/data/model/CharacterModel.kt
+++ app/src/main/java/com/kev/rickmorty/data/model/CharacterModel.kt
@@ -1,5 +1,5 @@
 package com.kev.rickmorty.data.model
 
 data class CharacterList(val results: ArrayList<CharacterModel>)
-data class CharacterModel(val id: Int, val name: String, val species: String, val image: String)
+data class CharacterModel(val id: Int, val name: String, val species: String, val image: String, val gender : String, val status : String)
 
--- app/src/main/java/com/kev/rickmorty/network/RetrofitService.kt
+++ app/src/main/java/com/kev/rickmorty/network/RetrofitService.kt
@@ -1,9 +1,10 @@
 package com.kev.rickmorty.network
 
-import android.telecom.Call
+import com.kev.rickmorty.data.model.CharacterList
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface RetrofitService {
     @GET("character")
-    fun getCharacters() : Call
+    fun getCharacters(): Call<CharacterList>
 }
